26d22ff40565dd43b2f302eccc8b5d96c35b5add
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2019-12-03 09:53:17 +0100

    Made RiskManagerController class

---
+++ 05 implementering/Risk-Manager/src/RiskManagerController.java
@@ -0,0 +1,20 @@
+public class RiskManagerController {
+    RiskTable rt = new RiskTable;
+    StrategyTable st = new StrategyTable;
+
+    public Risk addRisk() {
+        return Risk;
+    }
+
+    public void specifyRisk(Risk risk, String description, double probability, double consequence) {
+
+    }
+
+    public void linkStrategy(Risk risk, Strategy strategy) {
+
+    }
+
+    public void deleteRisk(Risk risk) {
+        
+    }
+}
