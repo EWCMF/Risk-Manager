afa15839d6386f39af2b3d7171dc0c48920a9cdc
Matiaslys <mati1857@edu.easj.dk>
2019-12-03 09:49:59 +0100

    Made Risk

---
+++ 05 implementering/Risk-Manager/src/Risk.java
@@ -0,0 +1,16 @@
+public class Risk {
+    String description;
+    double probability;
+    double consequence;
+    double exposure;
+    Strategy strategy = new Strategy;
+
+    public void specifyRisk(String description, double probability, double consequence) {
+
+
+    }
+    private void calculateExposure() {
+
+
+    }
+}
