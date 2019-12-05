b10a53bcc30f77255e41847bd1f36b97aa58a912
Matiaslys <mati1857@edu.easj.dk>
2019-12-05 08:39:35 +0100

    Database connection

--- 05 implementering/Risk-Manager/src/logic/RiskManagerController.java
+++ 05 implementering/Risk-Manager/src/logic/RiskManagerController.java
@@ -1,8 +1,27 @@
 package logic;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Button;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class RiskManagerController {
+    @FXML
+    private PasswordField User = new PasswordField();
+
+    @FXML
+    private PasswordField Pass = new PasswordField();
+
+    @FXML
+    private Button Confirm = new Button();
+
     RiskTable rt = new RiskTable();
     StrategyTable st = new StrategyTable();
+    private Statement stmt;
+
 
     public Risk addRisk() {
         return null;
@@ -19,4 +38,22 @@ public class RiskManagerController {
     public void deleteRisk(Risk risk) {
         rt.deleteRisk(risk);
     }
+
+
+    private void DBFacade() throws SQLException {
+
+        String password = User.getText();
+        String username = Pass.getText();
+        Connection connection = DriverManager.getConnection
+                ("jdbc:mysql://localhost/world?serverTimezone=UTC", username, password);
+        System.out.println("Database connected.");
+
+        // Create a statement
+        stmt = connection.createStatement();
+
+    }
+    private void CheckButton() {
+
+    }
 }
+
